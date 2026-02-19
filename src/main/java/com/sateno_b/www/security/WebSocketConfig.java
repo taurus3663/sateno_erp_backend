5f1e3758c5f02d3cd033ff479afa84618bdafa05
taurus366 <taurus.ali47@gmail.com>
2026-02-19 11:32:40 +0200

    .

--- src/main/java/com/sateno_b/www/security/WebSocketConfig.java
+++ src/main/java/com/sateno_b/www/security/WebSocketConfig.java
@@ -1,7 +1,9 @@
 package com.sateno_b.www.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -12,7 +14,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
+//        registry.enableSimpleBroker("/topic");
+        registry.enableSimpleBroker("/topic")
+                .setHeartbeatValue(new long[]{30000, 30000})
+                .setTaskScheduler(webSocketTaskScheduler());
 
         registry.setApplicationDestinationPrefixes("/app");
     }
@@ -20,5 +25,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+//        registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
+    }
+
+    @Bean
+    public ThreadPoolTaskScheduler webSocketTaskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(2);
+        scheduler.setThreadNamePrefix("ws-heartbeat-thread-");
+        scheduler.initialize();
+        return scheduler;
     }
 }
