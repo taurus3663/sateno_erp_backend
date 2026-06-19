a6e2471333f5523239882ad3b36cbfa5a50ddad1
developer <taurus.ali47@gmail.com>
2026-06-19 16:21:36 +0300

    .

--- src/main/java/com/sateno_b/www/service/SpeedyService.java
+++ src/main/java/com/sateno_b/www/service/SpeedyService.java
@@ -1044,10 +1044,6 @@ public class SpeedyService implements ShippingProvider {
                                 isUpdated = true;
                                 log.info("Order #{} marked as COMPLETED based on Speedy status: {}", order.getId(), desc);
                             }
-                            else if (desc.toLowerCase().contains("преглед") && order.getStatus() != OrderStatus.REFUSED_AFTER_REVIEW) {
-                                order.setStatus(OrderStatus.REFUSED_AFTER_REVIEW);
-                                isUpdated = true;
-                            }
                             else if(desc.toLowerCase().contains("връщане към подателя") || opCode.equals("111")) {
                                 if (order.getStatus() != OrderStatus.REFUSED_AFTER_REVIEW) {
                                     order.setStatus(OrderStatus.FAILED);
